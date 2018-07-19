4cab32bbae90bba0b4319e507626b8fff8a9c3bb
Philip Laureano <philip.laureano@gmail.com>
2018-07-19 17:11:18 +1000

    Added unicode roman numeral chars to the unit tests

--- RomanNumerals/RomanNumeralTests/RomanNumeralToIntegerConversionTests.cs
+++ RomanNumerals/RomanNumeralTests/RomanNumeralToIntegerConversionTests.cs
@@ -7,13 +7,18 @@ namespace RomanNumeralTests
     public class RomanNumeralToIntegerConversionTests
     {
         [Theory]
-        [InlineData("M",1000)]
-        [InlineData("D",500)]
-        [InlineData("C",100)]
-        [InlineData("L",50)]
-        [InlineData("X",10)]
-        [InlineData("V",5)]
-        [InlineData("I",1)]
+        [InlineData("ↈ", 100000)]
+        [InlineData("ↇ", 50000)]
+        [InlineData("ↂ", 10000)]
+        [InlineData("ↁ", 5000)]
+        [InlineData("ↀ", 1000)]
+        [InlineData("M", 1000)]
+        [InlineData("D", 500)]
+        [InlineData("C", 100)]
+        [InlineData("L", 50)]
+        [InlineData("X", 10)]
+        [InlineData("V", 5)]
+        [InlineData("I", 1)]
         public void TestConversion(string input, int expectedResult)
         {
             var converter = new Converter();
