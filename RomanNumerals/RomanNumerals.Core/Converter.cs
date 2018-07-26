893f7f06d5c92712f18a0731879d12b76114527e
Philip Laureano <philip.laureano@gmail.com>
2018-07-26 21:14:35 +1000

    Added implementation for converting a Roman Numeral string to an integer.

--- RomanNumerals/RomanNumerals.Core/Converter.cs
+++ RomanNumerals/RomanNumerals.Core/Converter.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
 
 namespace RomanNumerals.Core
 {
     public class Converter
     {
+        private static IDictionary<string, int> NumeralMap = new Dictionary<string, int>();
+
+        static Converter()
+        {
+            NumeralMap["M"] = 1000;
+            NumeralMap["D"] = 500;
+            NumeralMap["C"] = 100;
+            NumeralMap["L"] = 50;
+            NumeralMap["X"] = 10;
+            NumeralMap["V"] = 5;
+            NumeralMap["I"] = 1;
+        }
+
         public int Convert(string input)
         {
-            throw new NotImplementedException("TODO: Implement this method");
+            var reversedInput = input.ToUpperInvariant().Reverse();
+
+            var buffer = new Queue<char>(reversedInput.ToArray());
+            var currentValue = 0;
+            var total = 0;
+            var lastValue = 0;
+            while (buffer.Count > 0)
+            {
+                currentValue = GetNumeralValue(buffer.Dequeue());
+
+                if (lastValue <= currentValue)
+                    total += currentValue;
+                else
+                    total -= currentValue;
+
+                lastValue = currentValue;
+            }
+
+            return total;
+        }
+
+
+        private int GetNumeralValue(char ch)
+        {
+            return NumeralMap.ContainsKey(ch.ToString()) ? NumeralMap[ch.ToString()] : 0;
         }
 
         public string Convert(int input)
